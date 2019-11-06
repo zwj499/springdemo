7bdb1f40db1f8aa43edc84a3a7015f97bb6b6e5c
zhuangwj <499930612@qq.com>
2019-11-06 18:03:23 +0800

    project init

---
+++ src/main/java/com/example/springdemo/controller/TestController.java
@@ -0,0 +1,20 @@
+package com.example.springdemo.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @author zwj * @since 1.0
+ */
+@RestController
+@RequestMapping("/test")
+public class TestController {
+
+    @GetMapping
+    public String get() {
+        return "test";
+    }
+
+}
